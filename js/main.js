438276782efa982a5800afe89ddfe7343d47523c
Okonji Chukwunonso <davidokonji3@gmail.com>
2018-12-19 17:19:32 +0100

    modification of dashboard, added responsiveness to the dashboard and meetup file

---
+++ js/main.js
@@ -0,0 +1,94 @@
+// SIGNUP
+var show = document.getElementById('show');
+show.addEventListener('click',showPassword);
+
+function showPassword(){
+ var pass = document.getElementById('password'); 
+ if (pass.type === "password") {
+    pass.type = "text";
+  } else {
+    pass.type = "password";
+  }
+}
+//USER PROFILE TAB
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+document.getElementById("defaultOpen").click();
+
+//MEET UP
+
+// 'use strict';
+var btnAsk = document.querySelector(".btnAsk");
+var counter = document.querySelector("#num");
+var upvote = document.getElementById("up");
+var downvote = document.getElementById("down");
+var questions = document.getElementsByClassName("question_sess");
+var quesList = document.querySelector("#ques_list");
+var commentBtn = document.getElementById("commentBtn");
+var postBtn = document.getElementById("postCom");
+var commentPost = document.getElementById("commentPost");
+
+quesList.onclick = e => {
+  let action = e.target.dataset.action;
+  if (action == "up") {
+    e.target.parentNode.children[1].children[0].stepUp(1);
+  } else if (action == "down") {
+    e.target.parentNode.children[1].children[0].stepDown(1);
+  } else if (action == "addComment") {
+    //shows comment box
+    e.target.parentNode.children[1].style.display = "block";
+    e.target.parentNode.children[2].style.display = "block";
+  } else if (action == "postCom") {
+    // add a new comment
+    e.target.parentNode.parentNode.parentNode.children[1].innerText +=
+      e.target.parentNode.children[1].value;
+      // e.target.parentNode.children[1].innerText;
+    e.target.parentNode.parentNode.parentNode.children[1].style.display =
+      "block";
+  }
+};
+btnAsk.addEventListener("click", addQuestion);
+
+function addQuestion(e) {
+  e.preventDefault();
+  //picks the value in the question input box
+  var ques = document.getElementById("question").value;
+  //create a new DOM element in string format
+  let domManipulation = `<li class='question_tab' >
+      <div class="vote">
+        <button class="voteBtn" data-action = 'up'> <i class="fa fa-caret-up fa-3x" id="up"></i></button>
+        <div id="count">
+          <input type='number' min= 0 id="num" value="0" disabled/>
+        </div>
+        <button class="voteBtn" data-action = 'down'> <i class="fa fa-caret-down fa-3x" id="down" ></i></button>
+      </div>
+      <div class="question_sess" >
+        <!--modified here make changes in javascript -->
+        <div class = 'postQues'> 
+            ${ques}
+        </div>
+        <div id="commentPost">
+          
+        </div>
+        <div>
+          <div class="comment">
+            <a class='commentBtn' id="commentBtn" class="postC" data-action='addComment'>add a comment <i class='fa fa-comments fa-lg'></i></a>
+              <textarea class='commentBox' id='commentBox' ></textarea>
+            <button  id='postCom' data-action='postCom'>post comment</button>
+          </div>
+      </div>
+    </li>`;
+  //appends the question list
+  quesList.innerHTML += domManipulation;
+}
