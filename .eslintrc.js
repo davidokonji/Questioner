2befe37e2bc5edff0370d9ee438f025594c901ac
Okonji Chukwunonso <davidokonji3@gmail.com>
2018-12-30 22:53:47 +0100

    set up API endpoints and test suite for all endpoints

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,11 @@ module.exports = {
     "extends": "airbnb-base",
     "globals": {
         "__dirname": false
-    },
+    },"env": {
+        "mocha": true,
+        "node": true
+      },
+      "rules": {
+        "prefer-destructuring": ["error", {"object": false}]
+      }
 };
---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+
+import Questioner from './controller/questionerController';
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+app.post('/api/v1/meetups', Questioner.createMeetup);
+
+app.get('/api/v1/meetups/upcoming/', Questioner.getUpcoming);
+
+app.get('/api/v1/meetups/:id', Questioner.getMeetupById);
+
+app.get('/api/v1/meetups/', Questioner.getMeetups);
+
+app.post('/api/v1/questions', Questioner.createQuestion);
+
+app.patch('/api/v1/questions/:id/upvote', Questioner.patchQuestionUpvote);
+
+app.patch('/api/v1/questions/:id/downvote', Questioner.patchQuestionDownvote);
+
+app.post('/api/v1/meetups/:id/rsvps', Questioner.createRSVP);
+
+app.listen(port);
+
+export default app;
